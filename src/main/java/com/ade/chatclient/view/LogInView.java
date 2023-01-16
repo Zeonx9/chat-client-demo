841a52af536eabea79ab35095ee1d06ce8a24005
Egor <dementev.eg@yandex.ru>
2023-01-16 18:15:58 +0300

    logInPage

--- src/main/java/com/ade/chatclient/view/LogInView.java
+++ src/main/java/com/ade/chatclient/view/LogInView.java
@@ -1,12 +1,14 @@
 package com.ade.chatclient.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class LogInView {
 
     @FXML
     public TextField login_textfield;
+    public Label error_message;
 
     public void onLoginClick() {
         // there should be a transition to another screen after registration
