6affd2d5b95805e55408ed9b419bce96cac36dd6
Egor <dementev.eg@yandex.ru>
2023-01-16 17:00:38 +0300

    register page (now you can get user login)

--- src/main/java/com/ade/chatclient/view/RegisterView.java
+++ src/main/java/com/ade/chatclient/view/RegisterView.java
@@ -1,4 +1,14 @@
 package com.ade.chatclient.view;
 
+import javafx.scene.control.TextField;
+
 public class RegisterView {
+    public TextField login;
+
+    public String UserLogin;
+
+    public void registered() {
+        UserLogin = login.getText();
+        // there should be a transition to another screen after registration
+    }
 }
