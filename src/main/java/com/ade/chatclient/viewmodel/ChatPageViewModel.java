110eb947d38a5a39e53c1a3e2a85ec41d625dd0e
Zeonx9 <mushtukov.artem@mail.ru>
2023-01-18 12:47:59 +0300

    bind ChatPageView and realise view associated view logic

---
+++ src/main/java/com/ade/chatclient/viewmodel/ChatPageViewModel.java
@@ -0,0 +1,104 @@
+package com.ade.chatclient.viewmodel;
+
+import com.ade.chatclient.model.ClientModel;
+import com.ade.chatclient.model.entities.Chat;
+import com.ade.chatclient.model.entities.Message;
+import com.ade.chatclient.view.ViewHandler;
+import javafx.beans.Observable;
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.scene.control.ListCell;
+import lombok.Getter;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+public class ChatPageViewModel {
+    private final ListProperty<Chat> chatListProperty;
+    private final ListProperty<Message> messageListProperty;
+    private final StringProperty messageTextProperty;
+
+    private final ClientModel model;
+    private final ViewHandler viewHandler;
+
+    ChatPageViewModel(ViewHandler viewHandler, ClientModel model) {
+        this.model = model;
+        this.viewHandler = viewHandler;
+
+        chatListProperty = new SimpleListProperty<>(FXCollections.observableArrayList());
+        messageListProperty = new SimpleListProperty<>(FXCollections.observableArrayList());
+        messageTextProperty = new SimpleStringProperty();
+    }
+
+    public void updateChatList() {
+        chatListProperty.clear();
+        chatListProperty.addAll(model.getMyChats());
+    }
+
+    public void updateMessagesInSelectedChat() {
+        model.updateMessages();
+        messageListProperty.clear();
+        messageListProperty.addAll(model.getSelectedChatMessages());
+    }
+
+    public void onSelectedItemChange(Observable observable, Chat oldValue, Chat newValue) {
+        System.out.println("chat has been selected");
+        model.selectChat(newValue);
+        updateMessagesInSelectedChat();
+    }
+
+    public void sendMessage() {
+        System.out.println("sending message");
+        if (messageTextProperty.get().isBlank())
+            return;
+
+        model.sendMessageToChat(messageTextProperty.get());
+        messageTextProperty.set("");
+        updateMessagesInSelectedChat();
+    }
+
+    private String prepareChatToBeShown(Chat chat) {
+        List<String> memberNames = new ArrayList<>();
+        chat.getMembers().forEach(member -> {
+            if (!Objects.equals(member.getId(), model.getMyself().getId()))
+                memberNames.add(member.getName());
+        });
+        return String.join(", ", memberNames);
+    }
+
+    private String prepareMessageToBeShown(Message msg) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm, dd.MM");
+        return "from " + msg.getAuthor().getName() +
+                ": " + msg.getText() +
+                " at " + msg.getDateTime().format(dtf);
+    }
+
+    public ListCell<Chat> getChatListCellFactory() {
+        return new ListCell<>() {
+            @Override
+            protected void updateItem(Chat item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null)
+                    setText(null);
+                else
+                    setText(prepareChatToBeShown(item));
+            }
+        };
+    }
+
+    public ListCell<Message> getMessageCellFactory() {
+        return new ListCell<>() {
+            @Override
+            protected void updateItem(Message item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null)
+                    setText(null);
+                else
+                    setText(prepareMessageToBeShown(item));
+            }
+        };
+    }
+}
