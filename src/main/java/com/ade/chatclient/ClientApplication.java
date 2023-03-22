9fcb8b807a72c91caf863618aea492c5e6c7febe
Egor <dementev.eg@yandex.ru>
2023-03-22 20:25:59 +0300

    replace absolute path to app icon with relative path from resources

--- src/main/java/com/ade/chatclient/ClientApplication.java
+++ src/main/java/com/ade/chatclient/ClientApplication.java
@@ -5,6 +5,8 @@ import javafx.application.Application;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 /**
  * Точка входа в приложение, этот класс наследуется от класса JavaFX Application
  */
@@ -19,7 +21,7 @@ public class ClientApplication extends Application {
     @Override
     public void start(Stage stage) {
         stage.setTitle("InTouch");
-        Image image = new Image("C:/Users/ed230/IdeaProjects/chat-client-demo/src/main/resources/com/ade/chatclient/img/icon.png");
+        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("img/icon.png")));
         stage.getIcons().add(image);
         StartClientApp.start(stage);
     }
