aa5e757e59033fbee981fadae7a9fa9dfa04d0f3
Egor <dementev.eg@yandex.ru>
2024-01-02 13:21:01 +0300

    relocate GroupCreationDialog from AllChatsView to ChatPageView

--- src/main/java/com/ade/chatclient/viewmodel/AllChatsViewModel.java
+++ src/main/java/com/ade/chatclient/viewmodel/AllChatsViewModel.java
@@ -1,13 +1,11 @@
 package com.ade.chatclient.viewmodel;
 
+import com.ade.chatclient.application.ViewHandler;
 import com.ade.chatclient.application.structure.AbstractChildViewModel;
 import com.ade.chatclient.application.util.ListViewSelector;
-import com.ade.chatclient.application.ViewHandler;
 import com.ade.chatclient.application.util.ViewModelUtils;
 import com.ade.chatclient.domain.Chat;
-import com.ade.chatclient.dtos.GroupRequest;
 import com.ade.chatclient.model.ClientModel;
-import com.ade.chatclient.view.GroupCreationDialog;
 import com.ade.chatclient.view.cellfactory.ChatListCellFactory;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.SimpleListProperty;
@@ -15,16 +13,10 @@ import javafx.collections.FXCollections;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
-//import javafx.scene.media.Media;
-//import javafx.scene.media.MediaPlayer;
-
-import javafx.util.Duration;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.beans.PropertyChangeEvent;
-import java.io.File;
-import java.util.Optional;
 
 import static com.ade.chatclient.application.util.ViewModelUtils.listReplacer;
 import static com.ade.chatclient.application.util.ViewModelUtils.runLaterListener;
@@ -72,9 +64,9 @@ public class AllChatsViewModel extends AbstractChildViewModel<ClientModel> {
             if ((boolean) event.getOldValue()) {
                 selected = chat;
             }
-            else {
+//            else {
 //                playSound();
-            }
+//            }
         }
     }
 
@@ -129,19 +121,6 @@ public class AllChatsViewModel extends AbstractChildViewModel<ClientModel> {
         selector.select(selected);
         return factory;
     }
-
-    /**
-     * Метод создает и запускает диалоговое окно для создания новой беседы, после чего получает результат работы диалогового окна и отправляет данные в модель для создания нового группового чата
-     */
-    public void showDialogAndWait() {
-        GroupCreationDialog dialog = GroupCreationDialog.getInstance();
-        dialog.init(new GroupCreationDialogModel());
-        dialog.populateUserList(model.getAllUsers());
-
-        Optional<GroupRequest> answer = dialog.showAndWait();
-        answer.ifPresent(model::createGroupChat);
-    }
-
 
     /**
      * При изменении текста newText в поле для поиска вызывает
