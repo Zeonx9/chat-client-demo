dbcab1e7bb5b47ee3ce60e7c7c33451a3235c3c4
Egor <dementev.eg@yandex.ru>
2024-01-14 17:27:23 +0300

    division of the UserProfileView into the UserSettingsView and ProfileView

--- src/main/java/com/ade/chatclient/application/ViewModelProvider.java
+++ src/main/java/com/ade/chatclient/application/ViewModelProvider.java
@@ -16,7 +16,8 @@ public class ViewModelProvider {
     private final ModelFactory modelFactory;
     private AllUsersViewModel allUsersViewModel;
     private AllChatsViewModel allChatsViewModel;
-    private UserProfileViewModel userProfileViewModel;
+    private UserSettingsViewModel userProfileViewModel;
+    private ProfileViewModel profileViewModel;
     private AdminViewModel adminViewModel;
     private BackgroundService backgroundService;
 
@@ -30,7 +31,8 @@ public class ViewModelProvider {
         chatPageViewModel = new ChatPageViewModel(viewHandler, modelFactory.getModel());
         allUsersViewModel = new AllUsersViewModel(viewHandler, modelFactory.getModel());
         allChatsViewModel = new AllChatsViewModel(viewHandler, modelFactory.getModel());
-        userProfileViewModel = new UserProfileViewModel(viewHandler, modelFactory.getModel());
+        userProfileViewModel = new UserSettingsViewModel(viewHandler, modelFactory.getModel());
+        profileViewModel = new ProfileViewModel(viewHandler, modelFactory.getModel());
         adminViewModel = new AdminViewModel(viewHandler, modelFactory.getModel());
 
     }
--- src/main/java/com/ade/chatclient/viewmodel/UserSettingsViewModel.java
+++ src/main/java/com/ade/chatclient/viewmodel/UserSettingsViewModel.java
@@ -2,19 +2,19 @@ package com.ade.chatclient.viewmodel;
 
 import com.ade.chatclient.application.*;
 import com.ade.chatclient.application.structure.AbstractChildViewModel;
-import com.ade.chatclient.domain.User;
+import com.ade.chatclient.application.util.PaneSwitcher;
 import com.ade.chatclient.dtos.ChangePasswordRequest;
 import com.ade.chatclient.model.ClientModel;
 import com.ade.chatclient.view.ChangingPasswordDialog;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.scene.layout.Pane;
 import lombok.Getter;
 
 import java.beans.PropertyChangeEvent;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
+import static com.ade.chatclient.application.Views.PROFILE_VIEW;
 import static com.ade.chatclient.application.util.ViewModelUtils.runLaterListener;
 import static com.ade.chatclient.application.Views.LOG_IN_VIEW;
 
@@ -23,14 +23,11 @@ import static com.ade.chatclient.application.Views.LOG_IN_VIEW;
  * Регистрирует лисенер - "passwordChangeResponded"
  */
 @Getter
-public class UserProfileViewModel extends AbstractChildViewModel<ClientModel> {
-    private final StringProperty fullNameProperty = new SimpleStringProperty();
-    private final StringProperty birthDateProperty = new SimpleStringProperty();
-    private final StringProperty userNameProperty = new SimpleStringProperty();
-    private final StringProperty companyNameProperty = new SimpleStringProperty();
+public class UserSettingsViewModel extends AbstractChildViewModel<ClientModel> {
     private final StringProperty resultMessageProperty = new SimpleStringProperty();
+    private PaneSwitcher paneSwitcher;
 
-    public UserProfileViewModel(ViewHandler viewHandler, ClientModel model) {
+    public UserSettingsViewModel(ViewHandler viewHandler, ClientModel model) {
         super(viewHandler, model);
 
         model.addListener("passwordChangeResponded", runLaterListener(this::showRequestResult));
@@ -38,6 +35,7 @@ public class UserProfileViewModel extends AbstractChildViewModel<ClientModel> {
 
     /**
      * Устанавливает сообщение о результате смены пароля
+     *
      * @param event строка с результатом изменения пароля
      */
     private void showRequestResult(PropertyChangeEvent event) {
@@ -53,18 +51,6 @@ public class UserProfileViewModel extends AbstractChildViewModel<ClientModel> {
     }
 
     /**
-     * Метод получает из модели объект класса User пользователя и устанавливает личную информацию на экран
-     */
-    public void setUserPersonalData() {
-        User me = model.getMyself();
-
-        fullNameProperty.set(prepareFullName(me));
-        birthDateProperty.set(formatDOB(me.getDateOfBirth()));
-        userNameProperty.set(me.getUsername());
-        companyNameProperty.set(model.getCompany().getName());
-    }
-
-    /**
      * Метод создает и инициализирует диалоговое окно для смены пароля, отображает его на экране, после чего собирает данные от пользователя и отправляет их в модель для смены пароля
      */
     public void showDialogAndWait() {
@@ -77,38 +63,18 @@ public class UserProfileViewModel extends AbstractChildViewModel<ClientModel> {
     }
 
     /**
-     * @param date дата рождения пользователя
-     * @return дату рождения в формате "dd.MM.yyyy"
-     */
-    private String formatDOB(LocalDate date) {
-        if (date == null) {
-            return "-";
-        }
-        return date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
-
-    /**
-     * @param user объект класса User - пользователь
-     * @return имя и фамилию пользователя, если она указана
-     */
-    private String prepareFullName(User user) {
-        String raw = thisOrEmpty(user.getRealName()) + " " + thisOrEmpty(user.getSurname());
-        if (raw.isBlank()) {
-            return "-";
-        }
-        return raw.trim();
-    }
-
-    private String thisOrEmpty(String value) {
-        return value != null ? value : "";
-    }
-
-    /**
      * Метод запускает процесс выхода из аккаунта, очищает данные о пользователе в модели, останавливает работу BackgroundService и открывает окно входа в аккаунт
      */
     public void logOut() {
         model.clearModel();
         StartClientApp.stop();
         viewHandler.openView(LOG_IN_VIEW);
+    }
+
+    public void openUserProfile(Pane placeHolder) {
+        viewHandler.getViewModelProvider().getProfileViewModel().setUser(model.getMyself());
+
+        paneSwitcher = new PaneSwitcher(viewHandler, placeHolder);
+        paneSwitcher.switchTo(PROFILE_VIEW);
     }
 }
