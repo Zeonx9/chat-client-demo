73476ad3643171f362fd619bdfd03dbbd5b86a0f
Egor <dementev.eg@yandex.ru>
2024-05-19 14:20:59 +0300

    update GroupInfoDialog

--- src/main/java/com/ade/chatclient/view/GroupInfoDialog.java
+++ src/main/java/com/ade/chatclient/view/GroupInfoDialog.java
@@ -4,9 +4,8 @@ import com.ade.chatclient.application.structure.AbstractDialog;
 import com.ade.chatclient.domain.Chat;
 import com.ade.chatclient.domain.GroupChatInfo;
 import com.ade.chatclient.domain.User;
-import com.ade.chatclient.view.components.UserPhoto;
 import com.ade.chatclient.viewmodel.GroupInfoDialogModel;
-import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -15,7 +14,6 @@ import javafx.scene.layout.StackPane;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
@@ -31,23 +29,24 @@ public class GroupInfoDialog extends AbstractDialog<GroupChatInfo, GroupInfoDial
     @FXML private StackPane photoPane;
     @FXML private ListView<User> listMembers;
 
+    @Override
+    protected void initialize() {
+        groupName.textProperty().bind(viewModel.getGroupName());
+        systemMessage.textProperty().bind(viewModel.getSystemMessage());
+        countMembers.textProperty().bind(viewModel.getCountMembers());
+
+        listMembers.setCellFactory(param -> viewModel.getUserListCellFactory());
+        listMembers.itemsProperty().bind(viewModel.getUserListProperty());
+
+        Bindings.bindContentBidirectional(photoPane.getChildren(), viewModel.getPhotoNodes());
+    }
+
     /**
      * Вызывает метод инициализации из абстрактного класса, а так же устанавливает все значения в поля интерфейса диалогового окна
      * @param chat - объект класса Chat - беседа, информацию о которой показывает диалоговое окно
      */
     public void setChat(Chat chat) {
-        groupName.setText(chat.getGroup().getName());
-        countMembers.setText(chat.getMembers().size() + " members");
-        systemMessage.setText("");
-
-        listMembers.getItems().setAll(chat.getMembers());
-        listMembers.setCellFactory(param -> viewModel.getUserListCellFactory());
-
-        Objects.requireNonNull(UserPhoto.getPaneContent(chat, null, 40, viewModel.getImageRequest()))
-                .thenAccept(children -> Platform.runLater(() -> {
-                    photoPane.getChildren().clear();
-                    photoPane.getChildren().addAll(children);
-                }));
+        viewModel.setChat(chat);
     }
 
     public void setImageRequest(Function<String, CompletableFuture<Image>> imageRequest) {
@@ -57,9 +56,6 @@ public class GroupInfoDialog extends AbstractDialog<GroupChatInfo, GroupInfoDial
     public static GroupInfoDialog getInstance(){
         return AbstractDialog.getInstance(GroupInfoDialog.class, "group-info-dialog-view.fxml", "CellFactoryStyle");
     }
-
-    @Override
-    protected void initialize() {}
 
     @Override
     protected String getTitleString() {
--- src/main/java/com/ade/chatclient/viewmodel/GroupInfoDialogModel.java
+++ src/main/java/com/ade/chatclient/viewmodel/GroupInfoDialogModel.java
@@ -2,26 +2,39 @@ package com.ade.chatclient.viewmodel;
 
 import com.ade.chatclient.application.structure.AbstractDialogModel;
 import com.ade.chatclient.application.util.ViewModelUtils;
-import com.ade.chatclient.domain.EditChatResult;
-import com.ade.chatclient.domain.GroupChatInfo;
-import com.ade.chatclient.domain.User;
+import com.ade.chatclient.domain.*;
 import com.ade.chatclient.model.ClientModel;
 import com.ade.chatclient.view.EditChatDialog;
 import com.ade.chatclient.view.cellfactory.UserListCellFactory;
+import com.ade.chatclient.view.components.UserPhoto;
+import javafx.application.Platform;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListCell;
 import javafx.scene.image.Image;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
+@Getter
 public class GroupInfoDialogModel extends AbstractDialogModel<GroupChatInfo> {
     @Setter
-    @Getter
     private Function<String, CompletableFuture<Image>> imageRequest;
+    private final StringProperty groupName = new SimpleStringProperty();
+    private final StringProperty countMembers = new SimpleStringProperty();
+    private final StringProperty systemMessage = new SimpleStringProperty();
+    private final ListProperty<User> userListProperty = new SimpleListProperty<>(FXCollections.observableArrayList());
+    private final ObservableList<Node> photoNodes = FXCollections.observableArrayList();
     private final ClientModel model;
 
     public GroupInfoDialogModel(ClientModel model) {
@@ -32,6 +45,20 @@ public class GroupInfoDialogModel extends AbstractDialogModel<GroupChatInfo> {
     @Override
     public GroupChatInfo resultConverter(ButtonType buttonType) {
         return null;
+    }
+
+    public void setChat(Chat chat) {
+        groupName.set(chat.getGroup().getName());
+        countMembers.set(chat.getMembers().size() + " members");
+        systemMessage.set("");
+
+        userListProperty.setAll(chat.getMembers());
+
+        Objects.requireNonNull(UserPhoto.getPaneContent(chat, null, 40, imageRequest))
+                .thenAccept(children -> Platform.runLater(() -> {
+                    photoNodes.clear();
+                    photoNodes.addAll(children);
+                }));
     }
 
     public ListCell<User> getUserListCellFactory() {
@@ -53,7 +80,5 @@ public class GroupInfoDialogModel extends AbstractDialogModel<GroupChatInfo> {
             model.editGroupName(result.getChangeGroupName().getGroupName());
             //TODO answer.ifPresent(model::тут_функция_для_изменения_фото), ответ я даю вот такой: EditChatResult
         });
-
-
     }
 }
